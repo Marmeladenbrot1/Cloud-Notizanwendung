4c9dd4aff71dc3bd80011fbc3995370f0bdede45
Koch <L.Koch.inf22@stud.bbbaden.ch>
2024-03-22 09:53:55 +0100

    Update app.js for get und post function

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let button = document.getElementById("Add");
 let notiz;
+let text = document.getElementById("text");
 button.addEventListener('click', function() {
     newSite();
 });
@@ -27,13 +28,36 @@ function newSite(){
     })
 }
 
+function get() {
+    const URL = "https://api.endpoint.get.com"; 
+    const fetchData = {
+        method: "Post", 
+        body: input_text,
+         headers: { 
+         "Content-type": "application/json",
+         charset : "UTF-8" 
+     } 
+     };
+    
+    
+    fetch(URL, fetchData) 
+    .then((response) => { 
+        return response.json(); 
+    }) 
+    .then((fetchData) => { 
+         text.innerHTML = fetchData;
+    }).catch((error)=>{
+        console.log(error);
+    }); 
+}
+
 function push(){
         // instantiate a headers object
         var myHeaders = new Headers();
         // add content type header to object
         myHeaders.append("Content-Type", "application/json");
         // using built in JSON utility package turn object to string and store in a variable
-        var raw = JSON.stringify({"id":1,"notiz":Halllo});
+        var raw = JSON.stringify({"notiz": notiz});
         // create a JSON object with parameters for API call and store in a variable
         var requestOptions = {
             method: 'POST',
@@ -42,7 +66,7 @@ function push(){
             redirect: 'follow'
         };
         // make API call with parameters and use promises to get response
-        fetch("arn:aws:execute-api:us-east-1:891377119806:m7v0lwnbed/*/POST/", requestOptions)
+        fetch("https://api.endpoint.post.com", requestOptions)
         .then(response => response.text())
         .then(result => alert(JSON.parse(result).body))
         .catch(error => console.log('error', error));
